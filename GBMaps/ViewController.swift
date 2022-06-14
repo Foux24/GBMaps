8fcda4cbb1a1038c2cb239353d7ba3788373e9ee
Sukhoroslov_Vitalii <foux24@ya.ru>
2022-06-14 19:41:58 +0300

    Initial Commit

---
+++ GBMaps/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GBMaps
+//
+//  Created by Vitalii Sukhoroslov on 14.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
